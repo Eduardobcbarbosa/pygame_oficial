62ebce5224b6b3a2547febf91c0f9c4fc237544a
kauagenelhoud <kauavgg@al.insper.edu.br>
2025-05-14 14:46:59 -0300

    spawn inimigo e quiz

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import pygame
 import sys
-from config import *
+from config import WIDTH, HEIGHT, MENU, PHASE1, PHASE2, GAME_OVER, VICTORY
+from assets import fundodetela_menu, fundodetela_game_over
+from game_screen import draw_menu, draw_game_over
+#from sprites import Player, Bullet, Enemy, QuizWord, Heart
 
+
+pygame.init()
+pygame.mixer.init()
+
+
+game_state = MENU
 running = True
 clock = pygame.time.Clock()
 FPS = 60
@@ -16,9 +25,19 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
-
-    screen.fill(BLACK)
+            
+            if event.key == pygame.K_SPACE:
+                if game_state == MENU:
+                    game_state = PHASE1
+                elif game_state == GAME_OVER:
+                    game_state = MENU
+    
+    if game_state == MENU:
+        draw_menu()
+    elif game_state == GAME_OVER:
+        draw_game_over(None) 
+    
     pygame.display.flip()
-
 pygame.quit()
 sys.exit()
+
