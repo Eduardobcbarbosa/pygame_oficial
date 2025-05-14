1f013011c34ba2a84402b4a7c643f8ba257f4a88
Eduardo <eduardobcbarbosa@hotmail.com>
2025-05-14 14:28:53 -0300

    Todas as imagens inseridas

--- assets.py
+++ assets.py
@@ -4,3 +4,5 @@ pygame.init()
 
 fundodetela_menu = pygame.image.load('assets/tela_incial.png')
 fundodetela_phase1 = pygame.image.load('assets/tela_phase1.png')
+fundodetela_phase2 = pygame.image.load('assets/tela_phase2.png')
+fundodetela_game_over = pygame.image.load('assets/tela_game_over.png')
