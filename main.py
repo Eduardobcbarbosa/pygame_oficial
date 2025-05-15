6f5964578c3bfe43f99a16459e33de0421e90379
Eduardo <eduardobcbarbosa@hotmail.com>
2025-05-15 19:48:11 -0300

    game over funcional

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
 import pygame
 import sys
-from config import WIDTH, HEIGHT, MENU, PHASE1, PHASE2, GAME_OVER, VICTORY
-from assets import fundodetela_menu, fundodetela_game_over
-from game_screen import draw_menu, draw_game_over
-#from sprites import Player, Bullet, Enemy, QuizWord, Heart
-
 
 pygame.init()
 pygame.mixer.init()
 
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Guerra contra a DP")
 
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+MENU = 0
+PHASE1 = 1
+GAME_OVER = 2
 game_state = MENU
-running = True
-clock = pygame.time.Clock()
-FPS = 60
 
+assets = {
+    "FASE1_IMG": pygame.image.load('assets/tela_phase1.png'),
+    "MENU_IMG": pygame.image.load('assets/tela_inicial.png'),
+    "GAME_OVER_IMG": pygame.image.load('assets/tela_game_over.png')
+}
+
+def draw_menu():
+    screen.blit(assets["MENU_IMG"], (0, 0))
+
+def draw_game_over():
+    screen.blit(assets["GAME_OVER_IMG"], (0, 0))
+
+def draw_phase1():
+    screen.blit(assets["FASE1_IMG"], (0, 0))
+
+running = True
 while running:
-    clock.tick(FPS)
-    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -25,19 +40,21 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
-            
+
             if event.key == pygame.K_SPACE:
                 if game_state == MENU:
                     game_state = PHASE1
                 elif game_state == GAME_OVER:
                     game_state = MENU
-    
+
     if game_state == MENU:
         draw_menu()
     elif game_state == GAME_OVER:
-        draw_game_over(None) 
-    
+        draw_game_over()
+    elif game_state == PHASE1:
+        draw_phase1()
+
     pygame.display.flip()
+
 pygame.quit()
 sys.exit()
-
