092cbd353b4db2fd1b304d2f5ce11c5c85ef5fec
Eduardo <eduardobcbarbosa@hotmail.com>
2025-05-16 16:58:36 -0300

    erro de digitação em um dos assets

--- assets.py
+++ assets.py
@@ -15,7 +15,7 @@ GAME_OVER = 'tela_game_over'
 
 def load_assets():
     assets = {}
-    assets[BACKGROUND] = pygame.image.load('assets/tela_incial.png').convert()
+    assets[BACKGROUND] = pygame.image.load('assets/tela_inicial.png').convert()
     assets[FASE1_IMG] = pygame.image.load('assets/tela_phase1.png').convert()
     assets[FASE2_IMG] = pygame.image.load('assets/tela_phase2.png').convert()
     assets[GAME_OVER] = pygame.image.load('assets/tela_game_over.png').convert()
