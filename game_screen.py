f42666e77d3b757ec7e98cd5e1451f30035f1bb3
Luisa <luisabr1@al.insper.edu.br>
2025-05-19 16:24:04 -0300

    mudancas enquadramento imagens e cor de fonte

--- game_screen.py
+++ game_screen.py
@@ -4,21 +4,20 @@ from assets import fundodetela_menu, fundodetela_game_over, fundodetela_phase1, 
 
 def draw_menu():
     screen.blit(fundodetela_menu, (0, 0))  
-    title = font_xl.render("Guerra contra a DP", True, WHITE)
+
     start = font_medium.render("Pressione 'ESPAÇO' para começar", True, RED)
     instructions = font_medium.render("Use as setinhas para mover e 'ESPAÇO' para atirar", True, WHITE)
 
-    screen.blit(title, (WIDTH//2 - title.get_width()//2, HEIGHT//3))
+   
     screen.blit(start, (WIDTH//2 - start.get_width()//2, HEIGHT//2))
     screen.blit(instructions, (WIDTH//2 - instructions.get_width()//2, HEIGHT//2 + 50))
 
 def draw_game_over(player):
     screen.blit(fundodetela_game_over, (0, 0))  
-    text = font_large.render("GAME OVER", True, RED)
+    
     score_text = font_medium.render(f"Pontuação: {player.score}", True, WHITE)
     restart = font_medium.render("Pressione ESPAÇO para recomeçar", True, GREEN)
 
-    screen.blit(text, (WIDTH//2 - text.get_width()//2, HEIGHT//3))
     screen.blit(score_text, (WIDTH//2 - score_text.get_width()//2, HEIGHT//2))
     screen.blit(restart, (WIDTH//2 - restart.get_width()//2, HEIGHT//2 + 50))
 
@@ -37,8 +36,8 @@ def draw_phase1(player, bullets, enemies, quizzes):
         quiz.draw(screen)
 
     
-    score_text = font_medium.render(f"Pontos: {player.score}", True, BLACK)
-    lives_text = font_medium.render(f"Vidas: {player.lives}", True, BLACK)
+    score_text = font_medium.render(f"Pontos: {player.score}", True, WHITE)
+    lives_text = font_medium.render(f"Vidas: {player.lives}", True, WHITE)
     phase_text = font_small.render("Fase 1", True, BLACK)
 
     screen.blit(score_text, (20, 20))
